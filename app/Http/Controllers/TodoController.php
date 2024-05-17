66301ba11cfb915c995179c2b757709efb178593
muazkhairi92 <muazkhairi@alumni.iastate.edu>
2024-05-17 16:20:56 +0800

    update logics

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Todo;
 use App\Http\Requests\StoreTodoRequest;
 use App\Http\Requests\UpdateTodoRequest;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $todos = $request->session()->get('todos', []);
+
+        return view('todo', compact('todos'));    }
 
 
     /**
@@ -22,7 +24,13 @@ class TodoController extends Controller
      */
     public function store(StoreTodoRequest $request)
     {
-        //
+                $todos = $request->session()->get('todos', []);
+        
+                array_unshift($todos, $request->input('list'));
+        
+                $request->session()->put('todos', $todos);
+        
+                return redirect()->route('todo.index');
     }
 
 }
